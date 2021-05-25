4e15f4572b04dc87dff123976ee3ebc9033a2932
Ruben Rådelius <ruben.radelius@gmail.com>
2021-05-25 21:11:15 +0200

    added c++ project

---
+++ nim.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+    int playerScore = 0;
+    int aiScore = 0;
+
+    while (true) {
+        int matches = 24;
+        Random random = new Random;
+
+        cout << "Welcome to Nim, starting game..." << endl;
+
+        while (true) {
+
+        }
+    }
+}
