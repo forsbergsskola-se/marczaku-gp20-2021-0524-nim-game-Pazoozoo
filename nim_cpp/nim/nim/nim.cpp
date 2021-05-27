8d3f58d8a094958ca2544625cef9c81421e958c4
Ruben Rådelius <ruben.radelius@gmail.com>
2021-05-27 18:21:02 +0200

    reorganized functions so main is on top

--- nim_cpp/nim/nim/nim.cpp
+++ nim_cpp/nim/nim/nim.cpp
@@ -8,13 +8,86 @@ int aiScore = 0;
 int matches = 24;
 int drawnMatches = 0;
 
-void displayMatches() {
-    std::string matchesArt = "";
+bool playerTurn();
+bool aiTurn();
+bool playAgain();
+int playerInput(std::string message);
+void displayMatches();
 
-    for (int i = 0; i < matches; i++)
-        matchesArt += "|";
+int main() {
+    while (true) {
+        bool playing = 1;
+        matches = 24;
+        std::cout << "\nWelcome to Nim, starting new game...\n" << std::endl;
 
-    std::cout << "Matches: " << matchesArt << "\n" << std::endl;
+        while (playing) {
+            playing = playerTurn();
+
+            if (playing)
+                playing = aiTurn();
+        }
+
+        std::cout << "\nScore: \n" << "  Player: " << playerScore << "\n  AI: " << aiScore << std::endl;
+        if (playAgain)
+            continue;
+
+        std::cout << "\nExiting game..." << std::endl;
+        break;
+    }
+}
+
+bool playerTurn() {
+    std::string drawMessage = "Player, draw matches : [1, 2, or 3]";
+    drawnMatches = playerInput(drawMessage);
+    drawnMatches = std::clamp(drawnMatches, 1, 3);
+
+    if (drawnMatches > matches)
+        drawnMatches = matches;
+
+    std::cout << "Player drew " << drawnMatches << std::endl;
+    matches -= drawnMatches;
+
+    displayMatches();
+
+    if (matches > 0)
+        return true;
+
+    std::cout << "AI won!" << std::endl;
+    aiScore += 1;
+    return false;
+}
+
+bool aiTurn() {
+    switch (matches % 4) {
+    case 0:
+        drawnMatches = 3;
+        break;
+    case 1:
+        drawnMatches = std::rand() % 3 + 1;
+        break;
+    case 2:
+        drawnMatches = 1;
+        break;
+    case 3:
+        drawnMatches = 2;
+        break;
+    }
+    std::cout << "AI drew " << drawnMatches << std::endl;
+
+    matches -= drawnMatches;
+    displayMatches();
+
+    if (matches > 0)
+        return true;
+
+    std::cout << "Player won!" << std::endl;
+    playerScore += 1;
+    return false;
+}
+
+bool playAgain() {
+    std::string playAgainMessage = "\nEnter [1] to play again, or [2] to exit game";
+    return playerInput(playAgainMessage) != 2;
 }
 
 int playerInput(std::string message) {
@@ -38,78 +111,11 @@ int playerInput(std::string message) {
     return std::stoi(input);
 }
 
-bool playAgain() {
-    std::string playAgainMessage = "\nEnter [1] to play again, or [2] to exit game";
-    return playerInput(playAgainMessage) != 2;
-}
+void displayMatches() {
+    std::string matchesArt = "";
 
-bool playerTurn() {
-    std::string drawMessage = "Player, draw matches : [1, 2, or 3]";
-    drawnMatches = playerInput(drawMessage);
-    drawnMatches = std::clamp(drawnMatches, 1, 3);
+    for (int i = 0; i < matches; i++)
+        matchesArt += "|";
 
-    if (drawnMatches > matches)
-        drawnMatches = matches;
-
-    std::cout << "Player drew " << drawnMatches << std::endl;
-    matches -= drawnMatches;
-
-    displayMatches();
-
-    if (matches > 0) 
-        return true;
-    
-    std::cout << "AI won!" << std::endl;
-    aiScore += 1;
-    return false;
-}
-
-bool aiTurn() {
-    switch (matches % 4) {
-        case 0:
-            drawnMatches = 3;
-            break;
-        case 1:
-            drawnMatches = std::rand() % 3 + 1;
-            break;
-        case 2:
-            drawnMatches = 1;
-            break;
-        case 3:
-            drawnMatches = 2;
-            break;
-    }
-    std::cout << "AI drew " << drawnMatches << std::endl;
-
-    matches -= drawnMatches;
-    displayMatches();
-
-    if (matches > 0) 
-        return true;
-    
-    std::cout << "Player won!" << std::endl;
-    playerScore += 1;
-    return false;
-}
-
-int main() {
-    while (true) {
-        bool playing = 1;
-        matches = 24;
-        std::cout << "\nWelcome to Nim, starting new game...\n" << std::endl;
-
-        while (playing) {
-            playing = playerTurn();
-
-            if (playing)
-                playing = aiTurn();
-        }
-
-        std::cout << "\nScore: \n" << "  Player: " << playerScore << "\n  AI: " << aiScore << std::endl;
-        if (playAgain)
-            continue;
-
-        std::cout << "\nExiting game..." << std::endl;
-        break;
-    }
+    std::cout << "Matches: " << matchesArt << "\n" << std::endl;
 }
