7732cbd257ce527a1cf3f5880edbf7831db19a09
Ruben Rådelius <ruben.radelius@gmail.com>
2021-05-27 18:15:34 +0200

    improved playerInput function and used it for playAgain check

--- nim_cpp/nim/nim/nim.cpp
+++ nim_cpp/nim/nim/nim.cpp
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <algorithm>
 #include <string>
-#include <ctype.h>
-#include <sstream>
 
 int playerScore = 0;
 int aiScore = 0;
@@ -19,11 +17,35 @@ void displayMatches() {
     std::cout << "Matches: " << matchesArt << "\n" << std::endl;
 }
 
+int playerInput(std::string message) {
+    std::string input;
+    std::cout << message << std::endl;
+
+    while (true) {
+        std::cin >> input;
+
+        if (input.size() > 1) {
+            std::cout << "Invalid input, only one digit allowed!\n" << std::endl;
+            continue;
+        }
+
+        if (!isdigit(input[0])) {
+            std::cout << "Invalid input, only numbers allowed!\n" << std::endl;
+            continue;
+        }
+        break;
+    }
+    return std::stoi(input);
+}
+
+bool playAgain() {
+    std::string playAgainMessage = "\nEnter [1] to play again, or [2] to exit game";
+    return playerInput(playAgainMessage) != 2;
+}
+
 bool playerTurn() {
-    std::string input = "";
-    std::cout << "Player, draw matches: [1, 2, or 3]" << std::endl;
-    std::cin >> input;
-    drawnMatches = std::stoi(input);
+    std::string drawMessage = "Player, draw matches : [1, 2, or 3]";
+    drawnMatches = playerInput(drawMessage);
     drawnMatches = std::clamp(drawnMatches, 1, 3);
 
     if (drawnMatches > matches)
@@ -74,7 +96,6 @@ int main() {
     while (true) {
         bool playing = 1;
         matches = 24;
-
         std::cout << "\nWelcome to Nim, starting new game...\n" << std::endl;
 
         while (playing) {
@@ -83,15 +104,9 @@ int main() {
             if (playing)
                 playing = aiTurn();
         }
-        
+
         std::cout << "\nScore: \n" << "  Player: " << playerScore << "\n  AI: " << aiScore << std::endl;
-        std::cout << "\nEnter 1 to play again, or 2 to exit game" << std::endl;
-
-        std::string input = "";
-        std::cin >> input;
-        int response = std::stoi(input);
-
-        if (response != 2)
+        if (playAgain)
             continue;
 
         std::cout << "\nExiting game..." << std::endl;
