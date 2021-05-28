2619c624f6a1128fad8891d1dc6f9eba9e66b7ca
Ruben Rådelius <ruben.radelius@gmail.com>
2021-05-28 14:55:16 +0200

    added playerMove function and input validation for number and position on the board

--- tictacotoe_cpp/TicTacToe/TicTacToe/TicTacToe.cpp
+++ tictacotoe_cpp/TicTacToe/TicTacToe/TicTacToe.cpp
@@ -5,28 +5,50 @@ int board[9];
 bool player = 1;
 
 void printBoard();
+void playerMove(int);
 
 int main() {
-    printBoard();
+    while (true) {
+        playerMove(1);
+        playerMove(2);
+    }
+}
 
-    cout << "Player 1: Pick an open square [1-9]:";
+void playerMove(int player) {
+    printBoard();
+    cout << "Player " << player << ": Pick an open square [1-9] => ";
     int move = 0;
-    cin >> move;
-    move--;
-    board[move] = 1;
 
-    cout << "Player 2: Pick an open square [1-9]:";
-    cin >> move;
-    move--;
-    board[move] = 2;
+    while (true) {
+        cin >> move;
+        if (move < 1 || move > 9) {
+            cout << "Invalid input: number must be within 1 to 9 => ";
+            continue;
+        }
+        move--;
+        if (board[move] != 0) {
+            cout << "Invalid input: position already occupied, choose an open position => ";
+            continue;
+        }
+        break;
+    }
 
-    printBoard();
+    board[move] = player;
 }
 
 void printBoard() {
     for (int i = 0; i < 9; i += 3) {
         for (int j = i; j < i + 3; j++) {
-            cout << board[j];
+            int mark = 0;
+            switch (board[j]) {
+                case 1: 
+                    mark = 1; 
+                    break;
+                case 2: 
+                    mark = 2; 
+                    break;
+            }
+            cout << mark;
         }
         cout << "\n";
     }
