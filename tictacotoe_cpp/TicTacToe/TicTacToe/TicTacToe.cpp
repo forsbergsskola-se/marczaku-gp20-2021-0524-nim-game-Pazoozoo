884662e43e49970f7df65100768317ba6f72bacd
Ruben Rådelius <ruben.radelius@gmail.com>
2021-05-28 14:26:52 +0200

    added input for one turn

--- tictacotoe_cpp/TicTacToe/TicTacToe/TicTacToe.cpp
+++ tictacotoe_cpp/TicTacToe/TicTacToe/TicTacToe.cpp
@@ -2,10 +2,24 @@
 using namespace std;
 
 int board[9];
+bool player = 1;
 
 void printBoard();
 
 int main() {
+    printBoard();
+
+    cout << "Player 1: Pick an open square [1-9]:";
+    int move = 0;
+    cin >> move;
+    move--;
+    board[move] = 1;
+
+    cout << "Player 2: Pick an open square [1-9]:";
+    cin >> move;
+    move--;
+    board[move] = 2;
+
     printBoard();
 }
 
