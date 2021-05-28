45fbe8f118f5a68655d4752669e2bfed0d4c5eab
Ruben Rådelius <ruben.radelius@gmail.com>
2021-05-28 14:16:50 +0200

    added printBoard function

--- tictacotoe_cpp/TicTacToe/TicTacToe/TicTacToe.cpp
+++ tictacotoe_cpp/TicTacToe/TicTacToe/TicTacToe.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int board[9];
+
+void printBoard();
+
 int main() {
-    
+    printBoard();
 }
 
+void printBoard() {
+    for (int i = 0; i < 9; i += 3) {
+        for (int j = i; j < i + 3; j++) {
+            cout << board[j];
+        }
+        cout << "\n";
+    }
+}
